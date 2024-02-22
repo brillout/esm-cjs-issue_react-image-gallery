11c50253ea1659fbdaec62e3a72ec74d3d1482f6
Romuald Brillout <git@brillout.com>
2024-02-22 12:17:21 +0100

    Revert "workaround"
    This reverts commit 2e370cbf58a2b0a02d0822a2b5cfd37d09aa9d62.

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,9 @@
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
-import {cjsInterop} from "vite-plugin-cjs-interop";
-import {UserConfig} from 'vite'
+import { UserConfig } from 'vite'
 
-export default {
-  plugins: [
-    react(), vike(),
-    cjsInterop({
-      // List of CJS dependencies that require interop
-      dependencies: [
-        "react-image-gallery",
-      ],
-    }),
-  ],
-  /* Doesn't work:
-  ssr: {
-    noExternal: [
-         "react-image-gallery",
-    ],
-    // https://github.com/vitejs/vite/issues/14158#issuecomment-1685033558
-    optimizeDeps: {
-      include: [
-           "react-image-gallery",
-      ]
-    }
-  }
-  */
-} satisfies UserConfig;
+const config: UserConfig = {
+  plugins: [react(), vike()]
+}
+
+export default config
